b2965ca1bae6963d96f675efbb0f674ab729ad13
raheez <raheez@neovibe.in>
2018-06-27 21:08:12 +0530

    correcting game over rules

--- flappyBird.js
+++ flappyBird.js
@@ -18,7 +18,7 @@ bird.src = "images/bird.png";
 
 
 var gap = 85;
-var constant = pipeNorth.height+ gap;
+var constant;
 var bx = 10;
 var by = 150;
 var gravity = 1.5;
@@ -35,30 +35,52 @@ function moveup() {
     by -=25;
 }
 
+var score;
 function draw() {
-ctx.drawImage(bg,0,0);
 
-ctx.drawImage(bird,bx,by);
+    ctx.drawImage(bg,0,0);
 
-for (let i = 0; i < pipe.length; i++) {
-    ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
-    ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+ constant);
-    pipe[i].x -= 1;
+    for (let i = 0; i < pipe.length; i++) {
+        constant = pipeNorth.height+ gap;
+        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
+        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+ constant);
+        pipe[i].x --;
 
-    if (pipe[i].x==105) {
-        pipe.push({
-            x:cvs.width,
-            y:Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
-        })
+        if (pipe[i].x == 120) {
+            pipe.push({
+
+                x:cvs.width,
+                y:Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
+            });
+        }
+
+        //my version
+        if ((by<=pipe[i].height) && (bx+ bird.width >= pipe[i].x && bx+ bird.width <= pipe[i].x + pipeNorth.width  )  || (by+bird.height>=pipe[i].y+constant) && (bx+ bird.width >= pipe[i].x && bx+ bird.width <= pipe[i].x + pipeNorth.width )
+                || (by+bird.height>=cvs.height-fg.height)) {
+            location.reload(true);
+        }
+
+
+        //original 
+        // if( bx + bird.width >= pipe[i].x && bx <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || by+bird.height >= pipe[i].y+constant) || by + bird.height >=  cvs.height - fg.height){
+        //     location.reload(); // reload the page
+        // }
+
+
+        if (pipe[i].x==5) {
+            score++;
+        }
     }
-}
 
-ctx.drawImage(fg,0,cvs.height-fg.height);
+    
+    ctx.drawImage(bird,bx,by);
 
-by = by + gravity;
+    ctx.drawImage(fg,0,cvs.height-fg.height);
+
+    by = by + gravity;
 
 
-requestAnimationFrame(draw);
+    requestAnimationFrame(draw);
 
 }
 
